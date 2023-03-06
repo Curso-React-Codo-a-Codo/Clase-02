978bbcfcd0864fa4b005c0090945c662b5bc77bd
Javier Gil <javiergonzalog22@gmail.com>
2023-03-06 19:50:27 -0300

    Tercer Commit

--- Proyecto_Clase/js/app.js
+++ Proyecto_Clase/js/app.js
@@ -2,3 +2,7 @@ var a = 120; //Var para declarar variable. Cambia de tipo segun lo que se le asi
 var b = 550;
 
 console.log(a+b);
+
+var name = prompt("Cual es tu nombre?"); //Preguntar al usuario un dato y guardarlo en la variaable name
+
+alert("Hola " + name); //mensaje de alerta con el dato introducido
